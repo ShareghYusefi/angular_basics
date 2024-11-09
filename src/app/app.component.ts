36b370502b1c2ad9a5c6213c1ede36bf57cbe24e
Sharegh Yusefi <48734749+ShareghYusefi@users.noreply.github.com>
2024-11-08 17:57:11 -0700

    custom component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CustomComponent } from './custom/custom.component';
 
+// Component Decorator is used to define the metadata for the component
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CustomComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
---
+++ src/app/custom/custom.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'custom',
+  standalone: true,
+  imports: [],
+  templateUrl: './custom.component.html',
+  styleUrl: './custom.component.css',
+})
+export class CustomComponent {}
